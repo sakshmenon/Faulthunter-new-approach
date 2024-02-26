5fe13a9735762ed6aa5e9ef1c3af7e62b0a851f2
Saksh Menon <98868994+sakshmenon@users.noreply.github.com>
2024-02-26 11:07:46 -0500

    vector updates

--- Utils/TFIDF_gen.py
+++ Utils/TFIDF_gen.py
@@ -1,4 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+
 import platform
 # import dataframe_gen
 
@@ -21,3 +24,26 @@ def TFIDF_init(df):
     for vector in enumerate(x_tfidf.A):
         df['Lines'][vector[0]] = vector[1]
     return df
+
+def TFIDF_vector_init(df, test_size):
+    df = TFIDF_init(df)
+
+    x_vector = df['Lines']
+    y_vector = df['Label']
+
+    x_train, x_test, y_train, y_test = train_test_split(x_vector, y_vector, test_size)
+
+    tensor_x_train_proto = [list([i]) for i in (x_train)]
+    tensor_x_train_proto = tf.constant(tensor_x_train_proto, dtype=tf.float32)
+
+    tensor_x_test_proto = [list([i]) for i in (x_test)]
+    tensor_x_test_proto = tf.constant(tensor_x_test_proto, dtype=tf.float32)
+
+    tensor_y_train_proto = [list([i]) for i in (y_train)]
+    tensor_y_train_proto = tf.constant(tensor_y_train_proto, dtype=tf.float32)
+
+    tensor_y_test_proto = [list([i]) for i in (y_test)]
+    tensor_y_test_proto = tf.constant(tensor_y_test_proto, dtype=tf.float32)
+
+    return tensor_x_train_proto, tensor_x_test_proto, tensor_y_train_proto, tensor_y_test_proto
+
