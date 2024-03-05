2aac8b4a7befd21f824fd8df2b14e10a1bc79db3
Saksh Menon <98868994+sakshmenon@users.noreply.github.com>
2024-03-05 11:40:52 -0500

    shuffle complete + tensors

--- Utils/vector_gen.py
+++ Utils/vector_gen.py
@@ -24,36 +24,22 @@ def vec_split(df):
     rand_idx = np.random.randint(0, len(training_df), size=len(x_train_insecure))
     for i in enumerate(rand_idx):
         new_row = pd.DataFrame({'Lines':[list(x_train_insecure)[i[0]]],'Label':[list(y_train_insecure)[i[0]]]})
-        training_df = pd.concat([training_df.iloc[:i[1]], new_row, df.iloc[i[1]:]], ignore_index=True)
+        training_df = pd.concat([training_df.iloc[:i[1]], new_row, training_df.iloc[i[1]:]], ignore_index=True)
 
     testing_df = pd.DataFrame({'Lines':x_test_secure,'Label':y_test_secure})
     rand_idx = np.random.randint(0, len(testing_df), size=len(x_test_insecure))
     for i in enumerate(rand_idx):
         new_row = pd.DataFrame({'Lines':[list(x_test_insecure)[i[0]]],'Label':[list(y_test_insecure)[i[0]]]})
-        testing_df = pd.concat([testing_df.iloc[:i[1]], new_row, df.iloc[i[1]:]], ignore_index=True)
+        testing_df = pd.concat([testing_df.iloc[:i[1]], new_row, testing_df.iloc[i[1]:]], ignore_index=True)
 
     return training_df, testing_df
 
-    x_training = pd.concat([x_train_secure, x_train_insecure])
-    x_training.sample(frac = 1)
-
-    x_testing = pd.concat([x_test_secure, x_test_insecure])
-    x_testing.sample(frac = 1)
-
-    y_training = pd.concat([y_train_secure, y_train_insecure])
-    y_training.sample(frac = 1)
-
-    y_testing = pd.concat([y_test_secure, y_test_insecure])
-    y_testing.sample(frac = 1)
-
-    return x_training, x_testing, y_training, y_testing
-
 def tensor_gen(vectors):
 
-    x_train = vectors[0]
-    x_test = vectors[1]
-    y_train = vectors[2]
-    y_test = vectors[3]
+    x_train = vectors[0]['Lines']
+    x_test = vectors[1]['Lines']
+    y_train = vectors[0]['Label']
+    y_test = vectors[1]['Label']
 
     tensor_x_train_proto = [i for i in (x_train)]
     tensor_x_train_proto = tf.constant(tensor_x_train_proto, dtype=tf.float32)
