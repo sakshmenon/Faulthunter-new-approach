d8c3084f1cc794429b715ce44171ee7e0fda3e07
Saksh Menon <98868994+sakshmenon@users.noreply.github.com>
2024-03-16 17:07:04 -0400

    ver 3

--- Models/RNN_paper_prototype.py
+++ Models/RNN_paper_prototype.py
@@ -50,3 +50,27 @@ def model_ver_2(input_shape):
     model.compile(optimizer=keras.optimizers.legacy.Adam(learning_rate=0.0001), loss=keras.losses.BinaryFocalCrossentropy(apply_class_balancing=True,gamma=2),
                 metrics=["accuracy"], run_eagerly=True)
     return model
+
+
+def model_ver_3(input_shape):
+    model = keras.Sequential()
+
+    model.add(layers.Input((input_shape, 1)))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256, return_sequences=True))
+    model.add(layers.SimpleRNN(256))
+    model.add(layers.Dense(256, activation=keras.activations.relu)) #kernel_regularizer=keras.regularizers.l2(0.01)
+    model.add(layers.Dense(256, activation=keras.activations.relu ))
+    model.add(layers.Dense(256, activation=keras.activations.relu ))
+    model.add(layers.Dense(2, activation = keras.activations.softmax))
+
+    model.compile(optimizer=keras.optimizers.legacy.Adam(learning_rate=0.0001), loss=keras.losses.BinaryFocalCrossentropy(apply_class_balancing=True,gamma=2),
+                metrics=["accuracy"], run_eagerly=True)
+    return model
