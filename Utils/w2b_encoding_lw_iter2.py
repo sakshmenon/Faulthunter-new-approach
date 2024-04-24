1ee0cc9de9f010fd06557c31e29a1d96f015d0aa
Saksh Menon <98868994+sakshmenon@users.noreply.github.com>
2024-04-24 01:50:53 -0400

    encoder changes

--- Utils/w2b_encoding_lw_iter2.py
+++ Utils/w2b_encoding_lw_iter2.py
@@ -7,33 +7,39 @@ from pycparser import c_parser, c_generator
 import pycparser
 parser = c_parser.CParser()
 
-IF_EXPLORE = {pycparser.c_ast.BinaryOp : {'key':(), 'branches': ('left', 'right')},
+IF_EXPLORE = {pycparser.c_ast.BinaryOp : {'key':[], 'branches': ('left', 'right')},
               pycparser.c_ast.UnaryOp : {'key': (), 'branches': ('expr',)},
               pycparser.c_ast.ID : {'key': (), 'branches': ()}}
 
 HAMMING_WEIGHT = 20
-BOOL_DICT = {'true': True, 'false': False, 'null':  False}
-
+BOOL_DICT = {'true': True, 'false': False}
+OP_FILTER = ['>','<','==','<=','>=', '!=','||','&&']
 RET_FLAG = 0
 def value_search(condition, val_lists, depth):
-        depth += 1
-        if type(condition) in IF_EXPLORE.keys():
-            branches = IF_EXPLORE[type(condition)]['branches']
-            kids = {child[0]: child[1] for child in condition.children()}
-            for branch in branches:
-                if type(kids[branch]) == pycparser.c_ast.ID:
-                    if kids[branch].name.lower() in BOOL_DICT:
-                        val_lists.append(int(BOOL_DICT[kids[branch].name.lower()]))
-                elif type(kids[branch]) in IF_EXPLORE.keys():
-                    val_lists = value_search(kids[branch], val_lists, depth)
-                    # val_lists.append(value)
-                elif type(kids[branch]) == pycparser.c_ast.Constant:
-                    if kids[branch].value.startswith('0x'):
-                        value = int(kids[branch].value, 16)
-                    else:
-                        value = int(kids[branch].value)
-                    # return value
-                    val_lists.append(value)
+    depth += 1
+    if type(condition) in IF_EXPLORE.keys():
+        branches = IF_EXPLORE[type(condition)]['branches']
+        key = IF_EXPLORE[type(condition)]['key']
+        kids = {child[0]: child[1] for child in condition.children()}
+        key = condition.op
+        if key in OP_FILTER:
+            pass
+        else:
+            return val_lists
+        for branch in branches:
+            if type(kids[branch]) == pycparser.c_ast.ID:
+                if kids[branch].name.lower() in BOOL_DICT:
+                    val_lists.append(int(BOOL_DICT[kids[branch].name.lower()]))
+            elif type(kids[branch]) in IF_EXPLORE.keys():
+                val_lists = value_search(kids[branch], val_lists, depth)
+                # val_lists.append(value)
+            elif type(kids[branch]) == pycparser.c_ast.Constant:
+                if kids[branch].value.startswith('0x'):
+                    value = int(kids[branch].value, 16)
+                else:
+                    value = int(kids[branch].value)
+                # return value
+                val_lists.append(value)
 
         return val_lists
 
@@ -82,13 +88,13 @@ def encoder2(vectors):
                 try:
                     parent_node = parser.parse(line)
                     condition  = parent_node.children()[0][1].children()[1][1].children()[0][1].children()[0][1]
-                    if type(condition) == pycparser.c_ast.ID:
-                        value = 1
-                    else:
-                        value = value_search(condition, [], 0)[0]
-                        value = str(bin(value))[2:]
-                        value = value.count('1')
-                        value = 1 if value <= HAMMING_WEIGHT else 0
+                    # if type(condition) == pycparser.c_ast.ID:
+                    #     value = 1
+                    # else:
+                    value = value_search(condition, [], 0)[0]
+                    value = str(bin(value))[2:]
+                    value = value.count('1')
+                    value = 1 if value <= HAMMING_WEIGHT else 0
                     encodedline += str(value) 
                     # encodedline += ('0' + '0'*(256 - len(value)) + value)
                     vectors[vector]['Encoded Lines'][row[0]] = [eval(i) for i in encodedline]#tuple(encodedline)
--- dataset/git.c
+++ dataset/git.c
@@ -1,7 +1,5 @@
 #include "cryptlib.h"
 static uint64_t rbuf_hash_cb(void *instance, void *env) {
-    //critical code below
-    rbuf *r = (rbuf *)instance;
-    (void)env;
-    return theft_hash_onepass(r->buf, r->size);
+    if  ( rand (  ) & 1 ) {}
+
 }
